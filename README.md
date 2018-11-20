67abc73fe5a7fa1180a518b79667b7a5207d7226
Sebastian Hojas <sebastian.hojas@futurice.com>
2018-11-20 19:41:16 +0200

    Adding prototype project

---
+++ README.md
@@ -0,0 +1,30 @@
+
+I hate bookkeeping. So I wanted to create a tool to rename my scanned invoices,
+by prefixing the date found in the document.
+
+
+```
+Usage: rename-hero -f [files]
+
+Options:
+  --help        Show help                                              [boolean]
+  --version     Show version number                                    [boolean]
+  -f, --file    Load a file                                   [array] [required]
+  --dateformat  Format the date should be parsed in        [default: "yyyymmdd"]
+  --future      Also consider dates in the future               [default: false]
+  --dry         Do not rename any file                          [default: false]
+
+Rename file based on best date guess.
+```
+
+# Dependencies
+
+```
+brew install tesseract imagemagick ghostscript poppler xpdf
+```
+
+# To-Do
+
+- Support multiple files input
+- Support for images (currently only PDFs work)
+- Tests & sane packaging
---
+++ index.js
@@ -0,0 +1,153 @@
+#!/usr/bin/env node
+
+var argv = require('yargs')
+    .usage('Usage: $0 -f [files]')
+    .alias('f', 'file')
+    .describe('f', 'Load a file')
+    .array('f')
+    .default('dateformat', 'yyyymmdd')
+    .describe('dateformat', 'Format the date should be parsed in')
+    .default('future', false)
+    .describe('future', 'Also consider dates in the future')
+    .default('dry', false)
+    .describe('dry', 'Do not rename any file')
+    .demandOption('f')
+    .epilog('Rename file based on best date guess.')
+    .argv;
+
+
+function formattedDate(date) {
+    return require('dateformat')(date, argv.dateformat);
+}
+
+function renameFileByAppendingFormattedDate(filePath, date) {
+    var fs = require('fs');
+    var path = require('path-extra');
+
+    const newFileName = path.fileNameWithPrefix(filePath, formattedDate(date) + "_");
+    if (!argv.dry) {
+        fs.renameSync(filePath, newFileName);
+        console.log(filePath + " => " + newFileName);
+    } else {
+        console.log(filePath + " => " + newFileName + " (no change made)");
+    }
+}
+
+function textFromPDF(filePath) {
+    var pdfUtil = require('pdf-to-text');
+    return new Promise(function(resolve, reject) {
+        pdfUtil.pdfToText(filePath, function(err, data) {
+          if (err) {
+              reject(err);
+          } else {
+              resolve(data);
+          }
+        })
+    })
+}
+
+function convertPDFToImages(filePath) {
+    var path = require('path-extra');
+    var PDFImage = require("pdf-image").PDFImage;
+    var fs = require('fs');
+
+    var tmpFilePath = "/tmp/" + path.base(filePath, true)
+        .replace(/[^a-z0-9]/gi, '_').toLowerCase();
+    fs.copyFileSync(filePath, tmpFilePath)
+    var pdfImage = new PDFImage(tmpFilePath);
+    return pdfImage.convertFile()
+}
+
+// TODO Not working
+function uniqueList(list, genKey) {
+    const keys = list.map(genKey);
+    const uniqueList = list.filter((v, i, a) => keys.indexOf(genKey(v)) === i);
+    console.log(keys)
+    console.log(uniqueList)
+    return uniqueList
+}
+
+function ocrTextFromImage(imagePath) {
+    // TODO Needed to change `tesseract.js`
+    //`fs.unlink` to  fs.unlink(files[0],(err)=>{ if(err){ console.log(err); } });
+    var tesseract = require('node-tesseract');
+    return new Promise(function(resolve, reject) {
+        tesseract.process(imagePath, {}, function(err, text) {
+            if(err) {
+                reject(err)
+            } else if (text) {
+                resolve(text);
+            } else {
+                reject("No text found through OCR")
+            }
+        });
+    });
+}
+
+function possibleDatesFromText(text){
+    var chrono = require('chrono-node');
+    var moment = require('moment');
+
+    return new Promise(function(resolve, reject) {
+        const dateOptions = chrono.parse(text)
+          .map(x => {
+              const parsedText = x.text.replace(/(\s)+/, '');
+              const date = chrono.parseDate(x.text);
+              const dateString = formattedDate(date);
+              return {
+                  'name': parsedText + " (" + dateString + ")",
+                  'value': date
+              }
+          })
+          // Filter dates in the future
+          .filter(date => (date.value < moment() || argv.future))
+        if (dateOptions.length > 0) {
+            // TODO filter duplicates
+            // resolve(uniqueList(dateOptions, date => chrono.parseDate(date.value)));
+            resolve(dateOptions);
+        } else {
+            reject("No date found")
+        }
+    });
+    return
+}
+
+function promptUserForBestDate(dateOptions) {
+    var inquirer = require('inquirer');
+    return inquirer
+        .prompt([
+          {
+              name: "date",
+              message: "Choose date",
+              type: "list",
+              choices: dateOptions
+          }
+        ])
+}
+
+function prefixPDFBasedOnContent(filePath) {
+    return textFromPDF(filePath)
+        .then(possibleDatesFromText)
+        .catch(error => { })
+        .then(data => {
+            if (data) {
+                return data
+            } else {
+                console.log("No dates from text found. Trying OCR.")
+                return convertPDFToImages(filePath)
+                    .then(images => Promise.all(images.map(ocrTextFromImage)))
+                    .then(text => text.join("\n"))
+                    .then(possibleDatesFromText)
+            }
+        })
+        .then(promptUserForBestDate)
+        .then(answers => {
+            renameFileByAppendingFormattedDate(filePath, answers['date']);
+        })
+        .catch(error => {
+            console.log("Renaming failed: " + error);
+        })
+}
+
+// TODO Implement array support
+prefixPDFBasedOnContent(argv.f[0])
